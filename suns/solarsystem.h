a18493e432c26c98e602bb99c7965003e36ffbcc
UziZZZZ <79480311@qq.com>
2017-05-23 10:38:47 +0800

    基于opengl的一个太阳系运行动画

---
+++ suns/solarsystem.h
@@ -0,0 +1,34 @@
+﻿#ifndef solarsystem_h
+#define solarsystem_h
+
+
+#ifdef __APPLE__
+#include <gl/glut.h>
+#else
+#include <gl/glut.h>
+#endif
+
+#include "stars.h"
+
+#define STARS_NUM 10
+
+class SolarSystem {
+
+public:
+
+	SolarSystem();
+	~SolarSystem();
+
+	void onDisplay();
+	void onUpdate();
+	void onKeyboard(unsigned char key, int x, int y);
+
+private:
+	Star *stars[STARS_NUM];
+
+	GLdouble viewX, viewY, viewZ;
+	GLdouble centerX, centerY, centerZ;
+	GLdouble upX, upY, upZ;
+};
+
+#endif /* solarsystem_hpp */
---
+++ suns/stars.h
@@ -0,0 +1,47 @@
+﻿#ifndef star_h
+#define star_h
+
+#ifdef __APPLE__
+#include <gl/glut.h>
+#else
+#include <gl/glut.h>
+#endif
+
+class Star {
+public:
+	GLfloat radius;
+	GLfloat speed, selfSpeed;
+	GLfloat distance;
+	GLfloat rgbaColor[4];
+
+	Star* parentStar;
+
+	Star(GLfloat radius, GLfloat distance,
+		GLfloat speed, GLfloat selfSpeed,
+		Star* parent);
+	void drawStar();
+	virtual void draw() { drawStar(); }
+	virtual void update(long timeSpan);
+protected:
+	GLfloat alphaSelf, alpha;
+};
+
+class Planet : public Star {
+public:
+	Planet(GLfloat radius, GLfloat distance,
+		GLfloat speed, GLfloat selfSpeed,
+		Star* parent, GLfloat rgbColor[3]);
+	void drawPlanet();
+	virtual void draw() { drawPlanet(); drawStar(); }
+};
+
+class LightPlanet : public Planet {
+public:
+	LightPlanet(GLfloat Radius, GLfloat Distance,
+		GLfloat Speed, GLfloat SelfSpeed,
+		Star* Parent, GLfloat rgbColor[]);
+	void drawLight();
+	virtual void draw() { drawLight(); drawPlanet(); drawStar(); }
+};
+
+#endif /* star_hpp */
